6f00fde5c877e3611299492134f423fca7db3860
ROG <luan.roger.2003@gmail.com>
2025-02-14 14:35:55 -0300

    fix: CSS selector

--- src/services/status-invest/constants.ts
+++ src/services/status-invest/constants.ts
@@ -12,7 +12,7 @@ export const STATUS_INVEST_CLASS_SELECTORS = {
     ".top-info-sm-2 > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > a:nth-child(1) > strong:nth-child(1)",
   YIELD: {
     LAST_YIELD_VALUE:
-      "html body.main-nav-small main#main-2 div.container.pb-7 div.mt-5.d-flex.flex-wrap.flex-lg-nowrap.justify-between div#dy-info.chart.card.white-text.bg-main-gd.w-100.w-md-45.ml-lg-5.mr-lg-5.mt-3.mb-3.mt-md-0.mb-md-0 div.info div.d-flex.align-items-center strong.value.d-inline-block.fs-5.fw-900",
+      "#dy-info > div:nth-child(2) > div:nth-child(1) > strong:nth-child(2)",
     LAST_YIELD_PERCENTAGE:
       "#dy-info > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > b:nth-child(1)",
     LAST_YIELD_BASE_PRICE:
