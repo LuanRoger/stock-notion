31981f05d5e7797d437c05525b0c2e4f4f16bc8b
ROG <luan.roger.2003@gmail.com>
2025-02-26 19:26:20 -0300

    fix: fonts

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,23 +1,26 @@
 import ConfigureNotionField from "@/components/configure-notion-fields";
-import Footer from "@/components/footer";
+import HowToUseSection from "@/components/how-to-use-section";
 import NotionDatabaseForm from "@/components/notion-database-form";
+import Subtitle from "@/components/subtitle";
 import Title from "@/components/title";
 import { Card, CardContent } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center gap-5">
-      <Title />
-      <h2 className="text-muted-foreground font-mono text-lg text-center lg:text-left">
-        Preço de fundos imobiliarios direto no Notion.
-      </h2>
+    <div className="flex flex-col gap-5">
+      <div className="flex flex-col items-center gap-2">
+        <Title className="text-center" />
+        <Subtitle />
+      </div>
       <Card className="w-full">
         <CardContent>
           <NotionDatabaseForm />
         </CardContent>
       </Card>
       <ConfigureNotionField className="w-full" />
+      <Separator />
+      <HowToUseSection />
       <Separator />
     </div>
   );
