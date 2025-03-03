edd07d755542ed4c27f88fe5ffcb35cd5ce4075c
ROG <luan.roger.2003@gmail.com>
2025-03-03 14:53:49 -0300

    feat: create consumer function to update database

---
+++ apps/server/src/models/queue.ts
@@ -0,0 +1,18 @@
+import type { TimeZone } from "./utils-options";
+
+export interface UpdateNotionDatabaseFiMessage {
+  databaseId: string;
+  rowIdColumnName?: string;
+  actualValue?: string;
+  dividendYield?: string;
+  pvp?: string;
+  lastYieldValue?: string;
+  lastYieldPercentage?: string;
+  lastYieldBasePrice?: string;
+  lastYieldDate?: string;
+  nextYieldValue?: string;
+  nextYieldPercentage?: string;
+  nextYieldBasePrice?: string;
+  nextYieldDate?: string;
+  timeZone?: TimeZone;
+}
--- apps/server/src/modules/notion/queue.ts
+++ apps/server/src/modules/notion/queue.ts
@@ -1,11 +1,40 @@
-import { NOTION_DATABASE_FI_QUEUE } from "@/constants";
+import {
+  DEFAULT_NOTION_COLUMN_ID_NAME,
+  NOTION_DATABASE_FI_QUEUE,
+} from "@/constants";
+import type { UpdateNotionDatabaseFiMessage } from "@/models/queue";
 import { createConsumer } from "@/services/queue";
+import { createNotionClient } from ".";
+import { updateDatabaseFiisPageProperties } from "@/use-casses/notion";
+import type { NotionReducePropertiesOptions } from "@/models/utils-options";
 
 export function createNotionDatabaseFiQueueConsumer() {
-  return createConsumer(NOTION_DATABASE_FI_QUEUE, async (message) => {
-    console.log(message);
-  });
+  return createConsumer<UpdateNotionDatabaseFiMessage>(
+    NOTION_DATABASE_FI_QUEUE,
+    updateNotionDatabaseFi
+  );
 }
 
 export const notionDatabaseFiQueueConsumer =
   createNotionDatabaseFiQueueConsumer();
+
+async function updateNotionDatabaseFi(message: UpdateNotionDatabaseFiMessage) {
+  const notionSecret = process.env.NOTION_INTEGRATION_SECRET;
+  if (!notionSecret) {
+    return;
+  }
+
+  const notionClient = createNotionClient(notionSecret);
+  const { databaseId, rowIdColumnName, timeZone, ...databaseColumns } = message;
+  const databasePropertiesName =
+    Object.keys(databaseColumns).length !== 0 ? databaseColumns : undefined;
+  const reduceOptions: NotionReducePropertiesOptions = { timeZone };
+
+  await updateDatabaseFiisPageProperties(
+    notionClient,
+    databaseId,
+    rowIdColumnName ?? DEFAULT_NOTION_COLUMN_ID_NAME,
+    databasePropertiesName,
+    reduceOptions
+  );
+}
