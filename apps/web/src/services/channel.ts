c6f517cb895cdc9bfb1e6803807e255f388940d7
ROG <luan.roger.2003@gmail.com>
2025-10-13 09:03:10 -0300

    feat: update web to use Bun with he is Redis client

--- apps/web/src/services/channel.ts
+++
@@ -1,8 +0,0 @@
-import { createRedisClient } from "./redis";
-
-export async function publishToChannel(exchange: string, message: unknown) {
-  const redisClient = createRedisClient();
-  const jsonMessage = JSON.stringify(message);
-
-  await redisClient.publish(exchange, jsonMessage);
-}
